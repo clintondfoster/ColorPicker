be75a1336feaf5bcd602561cbdf5ad81ab27eb10
clinton foster <clintondavidfoster@gmail.com>
2023-08-30 14:50:15 -0400

    Completed. Ternary operator with selected color

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+
 // Write your Color component here
+const Color = ({color, setSelectedColor, selectedColor}) => {
+  return (
+  <div className={color}
+        onClick={()=> setSelectedColor(color)}></div>
+  )
+}
+
+// if {setSelectedColor} === {color} ? selectedBorder : {color}
+
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color={selectedColor === 'red' ? 'selected red' : 'red'}
+                setSelectedColor={setSelectedColor}/>
+        <Color color={selectedColor === 'violet' ? 'selected violet' : 'violet'}
+                setSelectedColor={setSelectedColor}/>
+        <Color color={selectedColor === 'blue' ? 'selected blue' : 'blue'}
+                setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
 
 export default App;
+
